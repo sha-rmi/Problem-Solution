118fbaade6d49cf3d9fdf41a2889131f1ca95d95
sha-rmi <sharmieswarank@gmail.com>
2026-03-11 23:37:59 +0530

    Implement method to find second largest number

---
+++ Day1/SecondLargestNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+       int largest = -1;
+        int secondLargest = -1;
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+
+            else if(arr[i] > secondLargest && arr[i] != largest){
+                secondLargest = arr[i];
+            }
+        }
+
+        return secondLargest;
+    }
+}
